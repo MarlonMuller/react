841d343462c48fd8589b595ad440671ab77d8867
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-04-04 21:01:12 -0300

    Update 04.04.24

--- primeiro-projeto-react/src/App.jsx
+++ primeiro-projeto-react/src/App.jsx
@@ -1,9 +1,30 @@
 
 function App() {
+  const list = [{id:12345, task:"Levar o Nico para passear"}, {id:"123456", task:"Terminar as aulas de React no DevClub"}]
 
+  //"Levar o Nico para passear", "Terminar as aulas de React no DevClub"
+
+  function inputMudou (event) {
+    console.log(event.target.value)
+  }
+
+  function cliqueiNoBotao() {
+    console.log("Cliquei")
+  }
+
+
+  // Retorna código HTML
   return (
       <div>
-        Hello React
+        <input onChange={inputMudou} placeholder="O que tenho para fazer" />
+        <button onClick={cliqueiNoBotao}>Adicionar</button>
+        <ul>
+          {
+          list.map(item => (
+            <li key={item.id}> {item.task}</li>
+          ))
+          }
+        </ul>
       </div>
   )
 }
