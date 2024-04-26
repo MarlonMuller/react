b697192c07960a9650ccba19f4ef96e1525eb495
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-04-25 21:24:03 -0300

    Update 25.04

--- projeto-dev-movies/src/routes/routes.jsx
+++ projeto-dev-movies/src/routes/routes.jsx
@@ -1,15 +1,17 @@
-import { Route, Routes} from 'react-router-dom';
-
+import { Route, Routes } from 'react-router-dom';
+import DefaultLayout from '../layout/DefaultLayout'
 import Home from '../containers/Home';
 import Movies from '../containers/Movies';
 import Series from '../containers/Series';
 
-function Router (){
+function Router() {
     return (
         <Routes>
-            <Route path='/' element = {<Home />} />
-            <Route path='/filmes' element= {<Movies />} />
-            <Route path='/series' element= {<Series />} />
+            <Route element={<DefaultLayout />}>
+                <Route path='/' element={<Home />} />
+                <Route path='/filmes' element={<Movies />} />
+                <Route path='/series' element={<Series />} />
+            </Route>
         </Routes>
     )
 }
